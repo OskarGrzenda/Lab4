e2d5adf378ac8d6a991b407ac0edbf2eb436895c
OSKAR GRZENDA - STUDENT <G00373428@gmit.ie>
2020-02-18 17:42:42 +0000

    lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count:number = 0;
+  hideLabel:boolean = true;
+
+
+  buttonPressed()
+  {
+    console.log("button pressed");
+    this.count++;
+  }
+
+  imageClicked()
+  {
+    console.log("Look at the star");
+    if  (this.hideLabel == true)
+    {
+      this.hideLabel = false;
+    }
+    else
+    {
+      this.hideLabel = true;
+    }
+    
+  }
+
+
+
 }
